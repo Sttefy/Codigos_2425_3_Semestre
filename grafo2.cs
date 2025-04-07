192c33916c671ddadf655135c0164ff61f8491e4
Sttefy <ms.gavilanezg@uea.edu.ec>
2025-04-06 22:39:52 -0500

    grafo 2

--- grafo2.cs
+++ grafo2.cs
@@ -1,20 +1,8 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-Console.WriteLine("Universidad Estatal Amazonica");
-Console.WriteLine("=====================================");
-Console.WriteLine("Mariuxi Stefania Gavilanez Gutierrez");
-Console.WriteLine("Y");
-Console.WriteLine("Luis Eduardo Argudo Guzman");
-Console.WriteLine("=====================================");
-Console.WriteLine("Tecnologias de la información");
-Console.WriteLine("=====================================");
-Console.WriteLine("Tercer Semestre");
-Console.WriteLine("=====================================");
-Console.WriteLine("Grafo de conexiones");
-Console.WriteLine("=====================================");
-
-
-Grafo grafo = new Grafo();
+namespace MiGrafo2
+{
+    class Grafo
+    {
+        Grafo grafo = new Grafo();
 
 grafo.AgregarVertice("Guayaquil");
 grafo.AgregarVertice("Quito");
@@ -60,5 +48,8 @@ class Grafo
             }
             Console.WriteLine();
         }
+    }
+}
+
     }
 }
